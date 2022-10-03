943eb78609c0edc5b19a565f49281a37b5273b1d
Chi Ntum <chi568@revature.net>
2022-10-03 10:53:15 -0500

    added new master branch after deleting revature git file

---
+++ HangMan/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string word = "demon";
+char[] letters=word.ToCharArray();
+int length = letters.Length;
+string[] wordpad;
+for( int i = 0; i < length; i++){
+    wordpad = wordpad + " _";
+}
+Console.WriteLine(wordpad);
